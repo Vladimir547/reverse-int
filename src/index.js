c14d6af3167b61b6fd05edc6641243f3416b1fc9
Vladimir547 <germes547@gmail.ru>
2020-02-21 01:53:31 +0300

    feat: solution reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result = [];
+  let str = `${n}`;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] != '-') {
+      result.push(str[i]);
+    }
+
+  }
+  result = result.join('');
+  return Number(result);
 }
